be7831a494038c4f4168193eab362f7c041de8f4
fuadbla008 <mdbarkatislamfuad@gmail.com>
2025-09-19 07:44:26 +0600

    added fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import React, { use, useMemo, useState } from 'react'
+import React, { use, useEffect, useMemo, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  // sample timeseries and category data
-  const salesSeries = [120, 200, 180, 240, 300, 280, 350, 420, 390, 450, 480, 520]
-  const userSeries = [40, 60, 55, 80, 90, 85, 120, 140, 130, 150, 160, 170]
-  const categoryData = [
-    { name: 'Product A', value: 420 },
-    { name: 'Product B', value: 280 },
-    { name: 'Product C', value: 160 },
-    { name: 'Product D', value: 90 },
-  ]
-  const recent = [
-    { id: 'TXN-1001', user: 'Alice', amount: 600, status: 'Completed' },
-    { id: 'TXN-1002', user: 'Bob', amount: 450, status: 'Pending' },
-    { id: 'TXN-1003', user: 'Carlos', amount: 780, status: 'Completed' },
-    { id: 'TXN-1004', user: 'Dana', amount: 60, status: 'Refunded' },
-  ]
+
+  const [users, getusers] = useState(0);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => getusers(data));
+  }, []);
+
+
 
   const [newCount, setFucn] = useState(0);
   const incrementCount = () => {
@@ -29,14 +23,6 @@ function App() {
       <h1>This is Counter Function</h1>
       <h3>Counter Number : {newCount}</h3>
       <button onClick={incrementCount}>incresse button</button>
-
-      <ul>
-        {categoryData.map(item => (
-          <li key={item.id}>
-            {item.name} - {item.value}
-          </li>
-        ))}
-      </ul>
     </div>
   )
 }
